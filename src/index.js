5df837a67002fba3f328d734583d9b0fd25f8f7e
blazecolour <936x936@gmail.com>
2018-06-30 12:52:07 +0300

    refactor parsers

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import render from './renderers';
 const genDiff = (file1, file2) => {
   const data1 = fs.readFileSync(file1, 'utf8');
   const data2 = fs.readFileSync(file2, 'utf8');
-  const fileExt = path.extname(file1).slice(1);
+  const fileExt = path.extname(file1);
   const obj1 = parsers(fileExt)(data1);
   const obj2 = parsers(fileExt)(data2);
   const ast = buildAst(obj1, obj2);
--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,15 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 
 const parsers = {
-  json: JSON.parse,
-  yml: yaml.safeLoad,
-  ini: ini.parse,
+  '.json': JSON.parse,
+  '.yml': yaml.safeLoad,
+  '.ini': ini.parse
 };
 
-export default fileExt => parsers[fileExt];
+export default format => data => {
+  const parse = parsers[format];
+  if (!parse) {
+    throw new Error('unkown format: ${format}');
+  }
+  return parse(data);
+};
