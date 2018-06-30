cdc19822531438ec1f638471a706ba89f534222f
blazecolour <936x936@gmail.com>
2018-06-30 12:32:28 +0300

    fix parsers

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const genDiff = (file1, file2) => {
   const data1 = fs.readFileSync(file1, 'utf8');
   const data2 = fs.readFileSync(file2, 'utf8');
   const fileExt = path.extname(file1).slice(1);
-  const obj1 = parsers[fileExt](data1);
-  const obj2 = parsers[fileExt](data2);
+  const obj1 = parsers(fileExt)(data1);
+  const obj2 = parsers(fileExt)(data2);
   const ast = buildAst(obj1, obj2);
   return render(ast);
 };
