71c74bcf78f123e234be7a100220270af08ac93a
blazecolour <936x936@gmail.com>
2018-07-01 08:19:31 +0300

    fix version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,11 +6,10 @@ import genDiff from '..';
 const program = commander;
 
 program
-  .version('1.1.0', '-V, --version')
+  .version('1.1.1', '-V, --version')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format [type]')
+  .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action((before, after, options) =>
-    console.log(genDiff(before, after, options.format)));
-
-program.parse(process.argv);
+  .action((before, after) =>
+    console.log(genDiff(before, after, program.format)))
+  .parse(process.argv);
